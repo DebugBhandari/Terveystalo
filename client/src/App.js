98c5abce2436e432b86582e6ecb52344905b7c03
Deepak <bhandarideepakdev@gmail.com>
2022-03-04 02:21:49 +0200

    local push3

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
     if (query) {
       const fetchData = async () => {
         const result = await axios(
-          `http://localhost/myapi/?action=${query.actionQuery}&number${
+          `http://localhost:5000/myapi/?action=${query.actionQuery}&number${
             query.actionQuery === "sumandcheck" ? "s" : ""
           }=${query.numbersQuery}`
         );
