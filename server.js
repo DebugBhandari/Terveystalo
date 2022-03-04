99cfb69cfaf7da266dc477ff8a7aaae6488429a3
Deepak <bhandarideepakdev@gmail.com>
2022-03-04 03:54:00 +0200

    local push5

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/myapi", (req, res) => {
 
 app.use(express.static('client/build'));
 app.get('*',(req, res) => {
-  res.sendFile(path.resolve(dirname, 'client', 'build','index.html'))
+  res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'))
 })
 
 app.listen(port, (err) => {
